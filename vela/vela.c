7fe8eac8da25d9a2ec99cecc0b2a1d1ea041607f
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-09-26 13:19:23 +0200

    Update vela.c

--- vela/vela.c
+++ vela/vela.c
@@ -1,1 +1,14 @@
-ve
+#include <stdio.h>
+int main(){
+  int i, j;
+  for(i=1;i<=10;i++)
+  {
+    for(j=1;j<=10;j++)
+    {
+      if(j<=i) printf("W");
+      else printf(".");
+    }
+  printf("\n");
+  }
+  return 0;
+}
