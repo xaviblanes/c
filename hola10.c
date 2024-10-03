69917a2ad1b0c5e43ca9a528079aad83da0dcb1c
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-03 19:26:12 +0200

    Update hola10.c

--- hola10.c
+++ hola10.c
@@ -1,8 +1,8 @@
-/***********************************
-*** programa en C que implementa ***
-*** la repetició determinada     ***
-*** Xavi Blanes curs:24/25       ***
-***********************************/
+/***************************
+programa en C que implementa
+la repetició determinada 
+Xavi Blanes curs:24/25  
+***************************/
 
 #include <stdio.h>
 int main(){
