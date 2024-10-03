83539a391db15a1b9433dc2b26b537390c7176c9
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-03 18:01:50 +0200

    Add files via upload

---
+++ capicua.c
@@ -0,0 +1,31 @@
+/********************************************
+*** programa en C que avalua si un número *** 
+*** introduït per teclat és capicua       ***
+*** Xavi Blanes curs:24/25                ***
+********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int n,      /* número llegit a avaluar */
+    unitats, /* les unitats de aux */
+    reves=0, /* serà n al revés */
+    aux;     /* un número auxiliar per a donar la volta a n */   
+
+   printf("Dona'm un número: ");
+   scanf("%d",&n);
+     
+   /* pose al revés n */
+   aux=n;   
+   while(aux!=0){
+      unitats=aux%10;
+      reves=reves*10+unitats;
+      aux=aux/10;
+   }
+   
+   /* compare n i reves */
+   if(n==reves) printf("El número %d és capicua\n",n);
+   else printf("El número %d no és capicua\n",n);
+   
+   getchar();
+}    
