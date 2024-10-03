8ab0573d59dc7643cd2b81ed991a3793a592e4f5
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-03 19:20:51 +0200

    Update holamon.c

--- holamon.c
+++ holamon.c
@@ -1,3 +1,9 @@
+/*****************************
+*** el meu primer          ***
+*** programa en C          ***
+*** Xavi Blanes curs:24/25 ***
+*****************************/
+
 #include <stdio.h>
 int main(){
 	printf("Hola món!!\n");
