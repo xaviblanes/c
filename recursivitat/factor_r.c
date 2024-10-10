6c354ebcee4b15cebe68144e6bc3ac8af759e68a
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-10 09:50:15 +0200

    Update factor_r.c

--- recursivitat/factor_r.c
+++ recursivitat/factor_r.c
@@ -1,1 +1,25 @@
+/*******************************************
+ * problema clàssic del factorial recursiu *
+ * Curiositat: 170 és el màxim factorial   * 
+ * representable amb double. Prova-ho      *
+ * Xavi Blanes curs: 24/25                 *
+ * ****************************************/
 
+#include <stdio.h>
+
+double factorial(int n);
+
+void main(){
+    int n;
+    printf("Introduix un número i et calcularé el factorial: ");
+    scanf("%d", &n);
+    printf("%f\n", factorial(n));
+    return;
+}
+
+double factorial(int n){
+    double fact;
+    if(n == 0) fact = 1;
+    else fact = n*factorial(n-1);
+    return fact;
+}
