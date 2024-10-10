728dd5b44dda6b2790603625ab846ba303f4cdef
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-10 09:40:22 +0200

    Update factor_iteratiu.c

--- recursivitat/factor_iteratiu.c
+++ recursivitat/factor_iteratiu.c
@@ -1,1 +1,25 @@
+/*******************************************
+ * problema clàssic del factorial iteratiu *
+ * Curiositat: 170 és el màxim factorial   * 
+ * representable amb double. Prova-ho      *
+ * Xavi Blanes curs: 24/25                 *
+ * ****************************************/
 
+#include <stdio.h>
+
+double factorial(int n);
+
+void main(){
+    int n;
+    printf("Introduix un número i et calcularé el factorial: ");
+    scanf("%d", &n);
+    printf("%f\n", factorial(n));
+    return;
+}
+
+double factorial(int n){
+    int i;
+    double fact = 1;
+    for(i=1;i<=n;i++) fact = fact*i;
+    return fact;
+}
